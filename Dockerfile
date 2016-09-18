6601d5de82557dff14869e3c2f4f24b025f4b9de
Daniel Stefaniuk <daniel.stefaniuk@gmail.com>
2016-09-18 13:31:36 +0100

    Include custom inputs.conf

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,10 @@ ENV SPLUNK_VERSION="6.4.3" \
     SPLUNK_BUILD="b03109c2bad4" \
     SPLUNK_URL="https://download.splunk.com/products/splunk/releases" \
     SPLUNK_HOME=/opt/splunk \
-    SPLUNK_PASSWORD="admin"
+    SPLUNK_PASSWORD="admin" \
+    SPLUNK_DEFAULT_ETC=/etc/splunk \
+    SPLUNK_USER_ETC=/usr/etc/splunk \
+    PATH="$PATH:/opt/splunk/bin"
 
 RUN set -ex \
     \
@@ -22,9 +25,13 @@ RUN set -ex \
     && (cd /tmp && md5sum -c $file.md5) \
     && mkdir -p $SPLUNK_HOME \
     && tar xzf /tmp/$file --strip 1 -C $SPLUNK_HOME \
+    && cp -R $SPLUNK_HOME/etc $SPLUNK_DEFAULT_ETC \
     \
     && rm -rf /tmp/* /var/tmp/* /var/lib/apt/lists/* /var/cache/apt/* \
     && rm -f /etc/apt/apt.conf.d/00proxy
+
+COPY assets/usr/etc/splunk /usr/etc/splunk
+ONBUILD COPY assets/usr/etc/splunk /usr/etc/splunk
 
 VOLUME [ "/opt/splunk/var" ]
 EXPOSE 1514 8000 8088 8089 8191 9997
--- assets/sbin/entrypoint.sh
+++ assets/sbin/entrypoint.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 set -e
 
+# Copy custom configuration files
+cp $SPLUNK_USER_ETC/system/local/inputs.conf $SPLUNK_HOME/etc/system/local/inputs.conf
+
 chown -R $SYSTEM_USER:$SYSTEM_USER $SPLUNK_HOME
 gosu $SYSTEM_USER $SPLUNK_HOME/bin/splunk start --answer-yes --no-prompt --accept-license
 trap "gosu $SYSTEM_USER $SPLUNK_HOME/bin/splunk stop" SIGINT SIGTERM EXIT
 
 if [ ! -f $SPLUNK_HOME/etc/.ui_login ]; then
 
-    # change password
+    # Change password
     gosu $SYSTEM_USER $SPLUNK_HOME/bin/splunk edit user admin \
         -auth admin:changeme \
         -password $SPLUNK_PASSWORD
@@ -20,32 +23,32 @@ if [ ! -f $SPLUNK_HOME/etc/.ui_login ]; then
     if [ -n "$SPLUNK_FORWARD_SERVER" ]; then
         gosu $SYSTEM_USER $SPLUNK_HOME/bin/splunk \
             add forward-server $SPLUNK_FORWARD_SERVER -auth admin:$SPLUNK_PASSWORD $SPLUNK_FORWARD_SERVER_ARGS
-        for n in {1..10}; do
-            if [[ -n $(eval echo \$\{SPLUNK_FORWARD_SERVER_${n}\}) ]]; then
-                gosu $SYSTEM_USER $SPLUNK_HOME/bin/splunk \
-                    add forward-server $(eval echo \$\{SPLUNK_FORWARD_SERVER_${n}\}) -auth admin:$SPLUNK_PASSWORD $(eval echo \$\{SPLUNK_FORWARD_SERVER_${n}_ARGS\})
-            else
-                break
-            fi
-        done
     fi
+    for n in {1..10}; do
+        if [[ -n $(eval echo \$\{SPLUNK_FORWARD_SERVER_${n}\}) ]]; then
+            gosu $SYSTEM_USER $SPLUNK_HOME/bin/splunk \
+                add forward-server $(eval echo \$\{SPLUNK_FORWARD_SERVER_${n}\}) -auth admin:$SPLUNK_PASSWORD $(eval echo \$\{SPLUNK_FORWARD_SERVER_${n}_ARGS\})
+        else
+            break
+        fi
+    done
 
     if [ -n "$SPLUNK_ADD" ]; then
         gosu $SYSTEM_USER $SPLUNK_HOME/bin/splunk \
             add $SPLUNK_ADD -auth admin:$SPLUNK_PASSWORD
-        for n in {1..30}; do
-            if [[ -n $(eval echo \$\{SPLUNK_ADD_${n}\}) ]]; then
-                gosu $SYSTEM_USER $SPLUNK_HOME/bin/splunk \
-                    add $(eval echo \$\{SPLUNK_ADD_${n}\}) -auth admin:$SPLUNK_PASSWORD
-            else
-                break
-            fi
-        done
     fi
+    for n in {1..30}; do
+        if [[ -n $(eval echo \$\{SPLUNK_ADD_${n}\}) ]]; then
+            gosu $SYSTEM_USER $SPLUNK_HOME/bin/splunk \
+                add $(eval echo \$\{SPLUNK_ADD_${n}\}) -auth admin:$SPLUNK_PASSWORD
+        else
+            break
+        fi
+    done
 
     echo -e "\n[license]\nactive_group = Free" >> /opt/splunk/etc/system/local/server.conf
 
-    # disable the "First time signing in?" message
+    # Disable the "First time signing in?" message
     touch $SPLUNK_HOME/etc/.ui_login
 fi
 
